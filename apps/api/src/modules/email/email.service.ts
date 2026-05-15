603a44c856ae0800bfa8571cfc3a33bc2d55d71e
zhixinsky <zhixinsky@gmail.com>
2026-05-15 11:26:26 +0800

    Fix nodemailer runtime import

--- apps/api/src/modules/email/email.service.ts
+++ apps/api/src/modules/email/email.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import nodemailer from 'nodemailer';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailService {
